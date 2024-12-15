dde05561dfd00d16f47da06c6f6befc16ee16ea6
gordanaleksandrovski20@gmail.com <gordanaleksandrovski20@gmail.com>
2024-12-15 15:48:44 +0100

    Added responsiveness, wrote the README.md

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
 import "./styles/globals.css";
 import Footer from "./components/navigation/Footer";
 import ClientLayout from "./components/global/ClientLayout";
+
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "Star Wars Films",
@@ -9,10 +10,11 @@ export const metadata: Metadata = {
   icons: {
     icon: "/icons/darth-vader.svg",
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
   themeColor: "#000000",
 };
 
